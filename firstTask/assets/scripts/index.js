5808030eba55744c43030ee08efc7648c6707399
Richard <rclancing@gmail.com>
2023-07-21 22:44:15 +0100

    index

--- firstTask/assets/scripts/index.js
+++ firstTask/assets/scripts/index.js
@@ -1,25 +1,34 @@
-const key = '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'
-$('#microphone-button').on("click", function () {
-  runSpeechRecog = () => {
-    $("entry").val("Loading text...");
-    var output = document.getElementById('entry');
-    // var action = document.getElementById('action');
-    let recognization = new webkitSpeechRecognition();
-    recognization.onstart = () => {
-      output.innerHTML = "Listening...";
-    }
-    recognization.onresult = (e) => {
-      output.innerHTML = e.results[0][0].transcript;
-    }
-    recognization.start();
+// Define the runSpeechRecog function in the global scope
+let runSpeechRecog = () => {
+  $("entry").val("Loading text...");
+  var output = document.getElementById('entry');
+  let recognition = new webkitSpeechRecognition();
+  recognition.onstart = () => {
+    output.innerHTML = "Listening...";
   }
-})
+  recognition.onresult = (e) => {
+    output.innerHTML = e.results[0][0].transcript;
+  }
+  recognition.start();
+};
+
+// Event Listener for Microphone Button
+$('#microphone-button').on("click", function () {
+  // Call the global runSpeechRecog function
+  runSpeechRecog();
+});
+
+
+
+
+
 // noinspection JSUnresolvedReference
 $("#convert-button").on("click", function () {
   // noinspection JSUnresolvedReference
   let input = $("#entry").val();
   conversion(input)
 });
+
 function conversion (input){
   // const instructions = 'These are the instructions:' +
   //     '1. Create spreadsheet formula based on user input' +
@@ -31,7 +40,7 @@ function conversion (input){
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${key}`,
+      Authorization: `Bearer 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`,
     },
     processData: false,
     data: JSON.stringify({
@@ -42,6 +51,18 @@ function conversion (input){
       top_p: 1.0,
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
+      // instructions: `${instructions}`,
+      // messages: {
+      //   0: {"prompt": "Add A1 to A9", "completion": "=SUM(A1:A9)"},
+      //   1: {
+      //     "prompt": "This is not a text to formula",
+      //     "completion": "Please enter a statement you want converted to a spreadsheet formula"
+      //   },
+      //   2: {
+      //     "prompt": "My name is Dave",
+      //     "completion": "Please enter a statement you want converted to a spreadsheet formula"
+      //   }
+      // },
     })
   };
 
@@ -50,3 +71,29 @@ function conversion (input){
     $("#result").val(response.choices[0].text);
   });
 }
+
+
+// const settings = {
+//   async: true,
+//   crossDomain: true,
+//   url: "https://api.openai.com/v1/completions",
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Authorization: `Bearer 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`,
+//   },
+//   processData: false,
+//   data: JSON.stringify({
+//     model: "text-davinci-003",
+//     prompt: `Correct this to standard English:\n\n${}.`,
+//     temperature: 0,
+//     max_tokens: 60,
+//     top_p: 1.0,
+//     frequency_penalty: 0.0,
+//     presence_penalty: 0.0,
+//   }),
+// };
+//
+// $.ajax(settings).done(function(response) {
+//     console.log(response);
+// });
